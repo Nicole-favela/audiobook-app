2f12c36a0176e061d8502d55c54861ced9a0f26c
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2024-04-21 16:56:17 -0600

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,24 @@
 
 import Home from './pages/Home'
 import Login from './pages/Login'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      {/* <Home /> */}
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path='/' exact />
+          <Route element={<Login />} path="/login" />
+
+        </Routes>
+
+      </BrowserRouter>
 
     </div>
   );
