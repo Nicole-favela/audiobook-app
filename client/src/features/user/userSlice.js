6139ee5c962165393957d53722b954f5a0a25dde
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2024-04-27 15:25:55 -0600

    added code for slice and store

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,5 +1,22 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = {
+    user: null
 
 }
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload;
+        },
+        logout: (state) => {
+            state.user = null;
+        }
+    }
+})
+
+export const { login, logout } = userSlice.actions;
+export const selectUser = (state) => state.action.user;
+export default userSlice.reducer;
