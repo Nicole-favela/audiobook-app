4cbd84de1c448959a04e093240dfa7af217f2b8f
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2024-05-07 14:18:54 -0600

    added handleClick function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Nav from '../components/Nav'
 import Book from '../components/Book'
 import { GlowUploadButton } from '../components/GlowUploadButton'
+import { useRef } from 'react'
 
 
 function Home() {
+    const fileInputRef = useRef(null);
+    const handleClick = () => {
+        fileInputRef.current.click(); // opens file dialog
+    };
+
+
     return (
         <div>
             <Nav />
-            <GlowUploadButton />
+            <GlowUploadButton onClick={handleClick} fileInputRef={fileInputRef} />
             <Book />
 
         </div>
