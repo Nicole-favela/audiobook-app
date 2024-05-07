566201988dbeb958a7481a7b028764ea2929eabf
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2024-05-07 13:07:56 -0600

    added upload button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Book from '../components/Book'
+import { GlowUploadButton } from '../components/GlowUploadButton'
 
 
 function Home() {
     return (
         <div>
             <Nav />
+            <GlowUploadButton />
             <Book />
 
         </div>
